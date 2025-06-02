0d611eab6329e2c48ce7064de945673cf121ecb3
StacyAkinyi <stacy.akinyi@gmail.com>
2025-06-02 19:50:56 +0300

    Fixing fortify

--- app/Actions/Fortify/UpdateUserPassword.php
+++ app/Actions/Fortify/UpdateUserPassword.php
@@ -21,9 +21,11 @@ class UpdateUserPassword implements UpdatesUserPasswords
         Validator::make($input, [
             'current_password' => ['required', 'string', 'current_password:web'],
             'password' => $this->passwordRules(),
-        ], [
-            'current_password.current_password' => __('The provided password does not match your current password.'),
-        ])->validateWithBag('updatePassword');
+        ])->after(function ($validator) use ($user, $input) {
+            if (! Hash::check($input['current_password'], $user->password)) {
+                $validator->errors()->add('current_password', __('The provided password does not match your current password.'));
+            }
+        })->validateWithBag('updatePassword');
 
         $user->forceFill([
             'password' => Hash::make($input['password']),
--- app/Http/Kernel.php
+++
@@ -1,5 +0,0 @@
-protected $routeMiddleware = [
-        //...
-        '2fa.setup' => \App\Http\Middleware\EnsureTwoFactorIsVerified::class,
-        '2fa.challenge' => \App\Http\Middleware\EnsureTwoFactorIsVerified::class,
-    ];
